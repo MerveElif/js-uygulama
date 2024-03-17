775a8ee21b4022ff59c3ac0158d5adacdb8f2f43
MerveElif <50811854+MerveElif@users.noreply.github.com>
2024-03-18 02:15:37 +0300

    added all validate and addeed phone input

--- form-validator/script.js
+++ form-validator/script.js
@@ -1,6 +1,7 @@
 const form = document.getElementById("form");
 const username = document.getElementById("username");
-const email = document.getElementById("email"); // Değişken adını emailInput olarak değiştirildi
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
 const password = document.getElementById("password");
 const repassword = document.getElementById("repassword");
 
@@ -33,8 +34,32 @@ function checkRequired(inputs) {
     }
   });
 }
+function checkLength(input, min, max) {
+  if (input.value.length < min) {
+    error(input, `${input.id} en az ${min} karakter olmalı`);
+  } else if (input.value.length > max) {
+    error(input, `${input.value} en çok ${max} karakter olmalı`);
+  } else {
+    success(input);
+  }
+}
+function checkPasswords(input1, input2) {
+  if (input1.value != input2.value) {
+    error(input2, "parolalar eşleşmiyor");
+  }
+}
+function checkPhone(input) {
+  var exp = /^\d{10}$/;
+  if (!exp.test(input.value)) {
+    error(input, "Telefon 10 karekterli olmalıdır");
+  }
+}
 
 form.addEventListener("submit", function (e) {
+  e.preventDefault();
   checkRequired([username, password, repassword, email]);
   checkEmail(email);
+  checkLength(username, 5, 12);
+  checkPasswords(password, repassword);
+  checkPhone(phone);
 });
