bc4b7fa439992ab72ed6229ef119d447b82c181b
MerveElif <50811854+MerveElif@users.noreply.github.com>
2024-03-19 21:24:22 +0300

    added local storage

--- sinema-bilet-rezervasyonu/script.js
+++ sinema-bilet-rezervasyonu/script.js
@@ -1,24 +1,71 @@
-const seats = document.querySelectorAll(".container .seat");
+const container = document.querySelector(".container");
 const count = document.getElementById("count");
 const amount = document.getElementById("amount");
 const select = document.getElementById("movie");
+const seats = document.querySelectorAll(".seat:not(.reserved)");
 
-seats.forEach((seat) => {
-  seat.addEventListener("click", function (e) {
-    if (!seat.classList.contains("reserved")) {
-      seat.classList.toggle("selected");
-      calculateTotal();
-    }
-  });
+getFromLocalStorage();
+calculateTotal();
+
+container.addEventListener("click", function (e) {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("reserved")
+  ) {
+    e.target.classList.toggle("selected");
+    calculateTotal();
+  }
 });
 
-select.addEventListener("change", function () {
+select.addEventListener("change", function (e) {
   calculateTotal();
 });
 
-const calculateTotal = function () {
-  const selectedSeats = document.querySelectorAll(".container .seat.selected");
-  count.textContent = selectedSeats.length;
-  let price = select.value;
-  amount.textContent = selectedSeats.length * price;
-};
+function calculateTotal() {
+  const selectedSeats = container.querySelectorAll(".seat.selected");
+
+  const selectedSeatsArr = [];
+  const seatsArr = [];
+
+  selectedSeats.forEach(function (seat) {
+    selectedSeatsArr.push(seat);
+  });
+
+  seats.forEach(function (seat) {
+    seatsArr.push(seat);
+  });
+
+  // [1,3,5]
+  let selectedSeatIndexs = selectedSeatsArr.map(function (seat) {
+    return seatsArr.indexOf(seat);
+  });
+
+  let selectedSeatCount = selectedSeats.length;
+  count.innerText = selectedSeatCount;
+  amount.innerText = selectedSeatCount * select.value;
+
+  saveToLocalStorage(selectedSeatIndexs);
+}
+
+function getFromLocalStorage() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+  if (selectedSeats != null && selectedSeats.length > 0) {
+    seats.forEach(function (seat, index) {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+  if (selectedMovieIndex != null) {
+    select.selectedIndex = selectedMovieIndex;
+  }
+}
+
+function saveToLocalStorage(indexs) {
+  localStorage.setItem("selectedSeats", JSON.stringify(indexs));
+  localStorage.setItem("selectedMovieIndex", select.selectedIndex);
+}
